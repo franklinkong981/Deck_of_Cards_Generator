dc0a07c0f3bd137455f0f6be65389f6206302f28
Franklin Kong <franklinkong981@gmail.com>
2024-07-09 23:51:29 -0700

    Finished project

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let cardCount = 0;
 let card_deck_id;
 const getCardButton = document.querySelector(".get-card");
+const cards = document.querySelector(".cards");
+const noMoreSign = document.querySelector(".no-more-sign");
 
 getCardButton.addEventListener('click', function() {
     if (card_deck_id) {
@@ -8,18 +10,36 @@ getCardButton.addEventListener('click', function() {
             .then(res => {
                 let value = res.data.cards[0].value;
                 let suit = res.data.cards[0].suit;
+                let image = res.data.cards[0].images.png;
                 console.log(`You drew the ${value} of ${suit} from deck ${card_deck_id}`);
+
+                //display card picture in cards div
+                const newCard = document.createElement("img");
+                newCard.setAttribute('src', image);
+                newCard.setAttribute('alt', `${value} of ${suit}`);
+                cards.append(newCard);
+
+                //if we run out of cards, don't allow user to generate another one.
                 if (res.data.remaining == 0) {
-                    console.log("There are no more cards left in this deck!");
+                    getCardButton.style.display = "none";
                 }
             })
+            .catch(err => console.log(err));
     } else {
         axios.get("https://deckofcardsapi.com/api/deck/new/draw/?count=1")
             .then(res => {
+                card_deck_id = res.data.deck_id;
+
                 let value = res.data.cards[0].value;
                 let suit = res.data.cards[0].suit;
-                card_deck_id = res.data.deck_id;
+                let image = res.data.cards[0].images.png;
                 console.log(`You drew the ${value} of ${suit} from deck ${card_deck_id}`);
+
+                //display card picture in cards div
+                const newCard = document.createElement("img");
+                newCard.setAttribute('src', image);
+                newCard.setAttribute('alt', `${value} of ${suit}`);
+                cards.append(newCard);
             })
             .catch(err => console.log(err));
     }
