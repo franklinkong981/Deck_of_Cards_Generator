64d1d5a7609c81c5d3d734e7810de2dbf139aad4
Franklin Kong <franklinkong981@gmail.com>
2024-07-17 15:14:03 -0700

    Used async/await instead of Promises

--- app.js
+++ app.js
@@ -4,43 +4,49 @@ const getCardButton = document.querySelector(".get-card");
 const cards = document.querySelector(".cards");
 const noMoreSign = document.querySelector(".no-more-sign");
 
-getCardButton.addEventListener('click', function() {
+getCardButton.addEventListener('click', async function() {
     if (card_deck_id) {
-        axios.get(`https://deckofcardsapi.com/api/deck/${card_deck_id}/draw/?count=1`)
-            .then(res => {
-                let value = res.data.cards[0].value;
-                let suit = res.data.cards[0].suit;
-                let image = res.data.cards[0].images.png;
-                console.log(`You drew the ${value} of ${suit} from deck ${card_deck_id}`);
+        try {
+            let res = await axios.get(`https://deckofcardsapi.com/api/deck/${card_deck_id}/draw/?count=1`);
+            let value = res.data.cards[0].value;
+            let suit = res.data.cards[0].suit;
+            let image = res.data.cards[0].images.png;
+            console.log(`You drew the ${value} of ${suit} from deck ${card_deck_id}`);
 
-                //display card picture in cards div
-                const newCard = document.createElement("img");
-                newCard.setAttribute('src', image);
-                newCard.setAttribute('alt', `${value} of ${suit}`);
-                cards.append(newCard);
+            //display card picture in cards div
+            const newCard = document.createElement("img");
+            newCard.setAttribute('src', image);
+            newCard.setAttribute('alt', `${value} of ${suit}`);
+            cards.append(newCard);
 
-                //if we run out of cards, don't allow user to generate another one.
-                if (res.data.remaining == 0) {
-                    getCardButton.style.display = "none";
-                }
-            })
-            .catch(err => console.log(err));
+            //if we run out of cards, don't allow user to generate another one.
+            if (res.data.remaining == 0) {
+                getCardButton.style.display = "none";
+                noMoreSign.innerText = "There are no more cards to draw!";
+            }
+        }
+        catch(e) {
+            console.log(e);
+        }
+
     } else {
-        axios.get("https://deckofcardsapi.com/api/deck/new/draw/?count=1")
-            .then(res => {
-                card_deck_id = res.data.deck_id;
+        try {
+            let res = await axios.get("https://deckofcardsapi.com/api/deck/new/draw/?count=1");
+            card_deck_id = res.data.deck_id;
 
-                let value = res.data.cards[0].value;
-                let suit = res.data.cards[0].suit;
-                let image = res.data.cards[0].images.png;
-                console.log(`You drew the ${value} of ${suit} from deck ${card_deck_id}`);
+            let value = res.data.cards[0].value;
+            let suit = res.data.cards[0].suit;
+            let image = res.data.cards[0].images.png;
+            console.log(`You drew the ${value} of ${suit} from deck ${card_deck_id}`);
 
-                //display card picture in cards div
-                const newCard = document.createElement("img");
-                newCard.setAttribute('src', image);
-                newCard.setAttribute('alt', `${value} of ${suit}`);
-                cards.append(newCard);
-            })
-            .catch(err => console.log(err));
+            //display card picture in cards div
+            const newCard = document.createElement("img");
+            newCard.setAttribute('src', image);
+            newCard.setAttribute('alt', `${value} of ${suit}`);
+            cards.append(newCard);
+        }
+        catch(e) {
+            console.log(e);
+        }
     }
 });
